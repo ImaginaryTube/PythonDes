2e7078355d6c11662443fd8705f8adb579017641
rob <a@b.com>
2019-05-22 01:28:51 -0700

    finished F-function and sbox now working on key funciotn

--- des.py
+++ des.py
@@ -33,11 +33,11 @@ def EncryptionRounds(permblock):
     roundnumber = 1
     while roundnumber <= 16:
         print("Round: ", roundnumber)
-        print("The Left bits:\t"+Leftbits)
-        print("The Right bits:\t"+Rightbits)
+        print("The Left bits:\t\t",Leftbits)
+        print("The Right bits:\t\t",Rightbits)
         rbtemp = Rightbits
-        #Rightbits = Leftbits ^ Ffuction(Rightbits)
-        Ffuction(Rightbits)
+        Rightbits = int(Leftbits, 2) ^ int(Ffunction(Rightbits), 2)
+        Rightbits = format(Rightbits, '032b')
         Leftbits = rbtemp
         roundnumber += 1
         
@@ -57,65 +57,89 @@ def Expansion(rbits):
     eperm = ''.join(eperm)
     return eperm
    
-def Ffuction(rightbits):
-    sb1 = [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+def Ffunction(rightbits):
+    sbox = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
             [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
             [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-            [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]]
-
-    sb2 = [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
-            [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
-            [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-            [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]]
+            [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
             
-    sb3 = [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+            [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+            [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+            [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+            [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
+            
+            [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
             [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
             [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
             [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]],
 
-    sb4 = [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+            [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
             [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
             [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
-            [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]]
+            [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]],
 
-    sb5 = [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+            [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
             [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
             [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
-            [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]]
+            [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]],
 
-    sb6 = [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+            [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
             [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
             [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
-            [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]]
+            [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]],
             
-    sb7 = [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+            [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
             [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
             [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
-            [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]]
+            [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]],
             
-    sb8 = [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+            [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
             [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
             [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
-            [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]
+            [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]]
 
     sboxinput = Expansion(rightbits)
+    #print("sbox expansion ", sboxinput)
     sboxinputsplit = [sboxinput[i:i+6] for i in range(0, len(sboxinput), 6)]
     
+    sboxnumber = 0
+    sboxoutput = []
     for bits in sboxinputsplit:
-        print("sbox input: "+bits)
+        #print("sbox {0} input: {1}".format(sboxnumber, bits))
         bits = list(bits)
         row = [bits.pop(0), bits.pop(-1)]
         coloumn = bits
         row = int(''.join(row),2)
         coloumn = int(''.join(coloumn),2)
-        print("\tcoloumn:",coloumn)
-        print("\trow:\t",row)
+        #print("\tcoloumn:",coloumn)
+        #print("\trow:\t",row)
+        sboxbin = format(sbox[sboxnumber][row][coloumn], '04b')
+        #print("\tsbox output: ", sboxbin)
+        sboxoutput.append(sboxbin)
+        sboxnumber += 1
+    ffunctionoutput = ''.join(sboxoutput)
+    print("ffunctionoutput output:\t", ffunctionoutput)
+    return ffunctionoutput
+    
+def Initalkeypermutaion(key):
+    ikpermtable = [57, 49, 41, 33, 25, 17, 9,
+                    1, 58, 50, 42, 34, 26, 18,
+                    10, 2, 59, 51, 43, 35, 27,
+                    19, 11, 3, 60, 52, 44, 36,
+                    63, 55, 47, 39, 31, 23, 15,
+                    7, 62, 54, 46, 38, 30, 22,
+                    14, 6, 61, 53, 45, 37, 29,
+                    21, 13, 5, 28, 20, 12, 4]
+                    
+    for position in ikpermtable:
+        keyperm.append(key[position-1])
+
+    
         
 if __name__ == '__main__':
     plaintextfile = open('plain.txt', 'r')
     plaintextbits = ''.join(format(ord(char), 'b') for char in plaintextfile.read())
-    print("Plaintext in bits: "+plaintextbits)
-    print()
+    print("Plaintext in bits: "+plaintextbits+"\n")
     bitlengthcheck = len(plaintextbits) % 64
 
     if bitlengthcheck != 0:

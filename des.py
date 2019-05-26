d4ed0362282b7a59b27f43f63144f12feaefe1f0
rob <a@b.com>
2019-05-25 21:28:58 -0700

    added gitignore. finished des but does not decrypted can only encrypt. moved vaibles to another file for clutter

--- des.py
+++ des.py
@@ -1,87 +1,33 @@
 #!/usr/bin/python
 
-def FinalPermutation(encyptedblock):
-    fptable = (40, 8, 48, 16, 56, 24, 64, 32,
-                39, 7, 47, 15, 55, 23, 63, 31,
-                38, 6, 46, 14, 54, 22, 62, 30,
-                37, 5, 45, 13, 53, 21, 61, 29,
-                36, 4, 44, 12, 52, 20, 60, 28,
-                35, 3, 43, 11, 51, 19, 59, 27,
-                34, 2, 42, 10, 50, 18, 58, 26,
-                33, 1, 41, 9, 49, 17, 57, 25)
+from varibles import *
 
 def EncryptionRounds(permblock):
     Leftbits, Rightbits = permblock[:32], permblock[32:]
     roundnumber = 1
     while roundnumber <= 16:
-        print("Round: ", roundnumber)
+        #print("Round: ", roundnumber)
         #print("The Left bits:\t\t",Leftbits)
         #print("The Right bits:\t\t",Rightbits)
-        rbtemp = Rightbits
-        Rightbits = int(Leftbits, 2) ^ int(Ffunction(Rightbits), 2)
-        Rightbits = format(Rightbits, '032b')
-        Leftbits = rbtemp
+        Leftbits, Rightbits = Rightbits, format((int(Leftbits, 2) ^ int(Ffunction(Rightbits, roundnumber), 2)), '032b')
         roundnumber += 1
+    final = Permutation(PII, Leftbits+Rightbits)
+    print("output block:\t\t", final)
+    return final
         
-def Ffunction(rightbits):
-    ExpansionTable = (32, 1, 2, 3, 4, 5,
-        4, 5, 6, 7, 8, 9,
-        8, 9, 10, 11, 12, 13,
-        12, 13, 14, 15, 16, 17,
-        16, 17, 18, 19, 20, 21,
-        20, 21, 22, 23, 24, 25,
-        24, 25, 26, 27, 28, 29,
-        28, 29, 30, 31, 32, 1)
-
-    sbox = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
-            [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
-            [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-            [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
-            
-            [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
-            [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
-            [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-            [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
-            
-            [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
-            [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
-            [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
-            [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]],
-
-            [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
-            [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
-            [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
-            [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]],
-
-            [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
-            [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
-            [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
-            [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]],
-
-            [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
-            [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
-            [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
-            [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]],
-            
-            [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
-            [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
-            [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
-            [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]],
-            
-            [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
-            [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
-            [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
-            [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]]
-
-    sboxinput = Permutation(ExpansionTable, rightbits)
-
-    #print("sbox expansion ", sboxinput)
+def Ffunction(rightbits, rn):
+    RN = rn
+    sboxinput = Permutation(SE, rightbits)
+    #print("sbox expansion:\t\t", sboxinput)
+    ikey = '1101001110101110010101001010101110101011010011101010001110100110'
+    sboxinput = int(sboxinput, 2) ^ int(keyfunc(ikey, RN), 2)
+    sboxinput = format(sboxinput, '048b')
+    #print("sboxinput:\t\t", sboxinput)
     sboxinputsplit = [sboxinput[i:i+6] for i in range(0, len(sboxinput), 6)]
-    
     sboxnumber = 0
     sboxoutput = []
     for bits in sboxinputsplit:
-        #print("sbox {0} input: {1}".format(sboxnumber, bits))
+        #print("sbox {0} input: {1}".format(sboxnumber+1, bits))
         bits = list(bits)
         row = [bits.pop(0), bits.pop(-1)]
         coloumn = bits
@@ -89,27 +35,45 @@ def Ffunction(rightbits):
         coloumn = int(''.join(coloumn),2)
         #print("\tcoloumn:",coloumn)
         #print("\trow:\t",row)
-        sboxbin = format(sbox[sboxnumber][row][coloumn], '04b')
-        #print("\tsbox output: ", sboxbin)
-        sboxoutput.append(sboxbin)
+        sboxin = format(Sbox[sboxnumber][row][coloumn], '04b') 
+        #print("\tsboxoutput: ", sboxin)
+        sboxoutput.append(sboxin)
         sboxnumber += 1
     ffunctionoutput = ''.join(sboxoutput)
-    print("ffunctionoutput output:\t", ffunctionoutput)
+    ffunctionoutput = Permutation(P, ffunctionoutput)
+    #print("ffunctionoutput output:\t", ffunctionoutput)
     return ffunctionoutput
     
-def keyfunc(key):
-    print("input key", key)
-    PCTable = [57, 49, 41, 33, 25, 17, 9,
-                1, 58, 50, 42, 34, 26, 18,
-                10, 2, 59, 51, 43, 35, 27,
-                19, 11, 3, 60, 52, 44, 36,
-                63, 55, 47, 39, 31, 23, 15,
-                7, 62, 54, 46, 38, 30, 22,
-                14, 6, 61, 53, 45, 37, 29,
-                21, 13, 5, 28, 20, 12, 4]
-                    
-    keyperm = Permutation(PCTable, key)
-    leftkey, rightkey = keyperm[28:], keyperm[:28]
+def keyfunc(key, RoundNumber):
+    #print("input key:\t\t", key)
+    SingleShiftRounds = [1, 2, 9, 16]
+
+    keyperm = Permutation(PC, key)
+    #print("permkey:\t\t", keyperm)
+    leftkey, rightkey = keyperm[:28], keyperm[28:]
+    #print("leftkey:\t\t", leftkey)
+    #print("rightkey:\t\t", rightkey)
+    leftkey = list(leftkey)
+    rightkey = list(rightkey)
+    if any(SingleShiftRounds) == RoundNumber:
+        #print("Single shift")
+        leftkey.append(leftkey.pop(0))
+        rightkey.append(rightkey.pop(0))
+        #print("leftkey:\t\t {}".format(''.join(leftkey)))
+        #print("rightkey:\t\t {}".format(''.join(rightkey)))
+    else:
+        #print("double shift")
+        leftkey.append(leftkey.pop(0))
+        leftkey.append(leftkey.pop(0))
+        rightkey.append(rightkey.pop(0))
+        rightkey.append(rightkey.pop(0))
+        #print("leftkey:\t\t {}".format(''.join(leftkey)))
+        #print("rightkey:\t\t {}".format(''.join(rightkey)))
+    joinkey = leftkey+rightkey
+    subkey = ''.join(joinkey)
+    subkey = Permutation(PC2, subkey)
+    #print("subkey:\t\t\t",subkey)
+    return subkey
 
 def Permutation(table, bits):
     permbits = []
@@ -119,25 +83,16 @@ def Permutation(table, bits):
     return permbits
 
 if __name__ == '__main__':
-    IPTable = (58, 50, 42, 34, 26, 18, 10, 2,
-                60, 52, 44, 36, 28, 20, 12, 4,
-                62, 54, 46, 38, 30, 22, 14, 6,
-                64, 56, 48, 40, 32, 24, 16, 8,
-                57, 49, 41, 33, 25, 17, 9, 1,
-                59, 51, 43, 35, 27, 19, 11, 3,
-                61, 53, 45, 37, 29, 21, 13, 5,
-                63, 55, 47, 39, 31, 23, 15, 7)
-
     plaintextfile = open('plain.txt', 'r')
     plaintextbits = ''.join(format(ord(char), 'b') for char in plaintextfile.read())
-    #print("Plaintext in bits: "+plaintextbits+"\n")
+    print("Plaintext in bits: "+plaintextbits+"\n")
     bitlengthcheck = len(plaintextbits) % 64
-
     if bitlengthcheck != 0:
         print("Input string not correct length must be 64bit blocks")
         print("Last block currently at: ", bitlengthcheck)
-
     plaintextblocks = [plaintextbits[i:i+64] for i in range(0, len(plaintextbits), 64)]
-
+    cyphertext = []
     for block in plaintextblocks:
-        EncryptionRounds(Permutation(IPTable, block))
+        print("input block: \t\t", block)
+        cyphertext.append(EncryptionRounds(Permutation(PI, block)))
+    print(''.join(cyphertext))

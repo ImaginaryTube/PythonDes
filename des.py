0305f70892e66006be3612c720bcd0688f099b82
rob <a@b.com>
2019-05-31 22:03:47 -0700

    made cipher text written to a file not printed and made key function self sufficient and out put all keys as a list.

--- des.py
+++ des.py
@@ -2,26 +2,25 @@
 
 from varibles import *
 
-def EncryptionRounds(permblock):
+def EncryptionRounds(permblock, key):
     Leftbits, Rightbits = permblock[:32], permblock[32:]
     roundnumber = 1
+    keyfunc(key)
     while roundnumber <= 16:
         #print("Round: ", roundnumber)
         #print("The Left bits:\t\t",Leftbits)
         #print("The Right bits:\t\t",Rightbits)
-        Leftbits, Rightbits = Rightbits, format((int(Leftbits, 2) ^ int(Ffunction(Rightbits, roundnumber), 2)), '032b')
+        Leftbits, Rightbits = Rightbits, format((int(Leftbits, 2) ^ int(Ffunction(Rightbits, key[roundnumber-1]), 2)), '032b')
         roundnumber += 1
     final = Permutation(IPI, Leftbits+Rightbits)
-    print("output block:\t\t", final)
-    print()
+    #print("output block:\t\t", final)
+    #print()
     return final
         
-def Ffunction(rightbits, rn):
-    RN = rn
+def Ffunction(rightbits, subkey):
     sboxinput = Permutation(SE, rightbits)
     #print("sbox expansion:\t\t", sboxinput)
-    ikey = '1101001110101110010101001010101110101011010011101010001110100110'
-    sboxinput = int(sboxinput, 2) ^ int(keyfunc(ikey, RN), 2)
+    sboxinput = int(sboxinput, 2) ^ int(subkey)
     sboxinput = format(sboxinput, '048b')
     #print("sboxinput:\t\t", sboxinput)
     sboxinputsplit = [sboxinput[i:i+6] for i in range(0, len(sboxinput), 6)]
@@ -43,12 +42,12 @@ def Ffunction(rightbits, rn):
     ffunctionoutput = ''.join(sboxoutput)
     ffunctionoutput = Permutation(P, ffunctionoutput)
     #print("ffunctionoutput output:\t", ffunctionoutput)
-    return ffunctionoutput
-    
-def keyfunc(key, RoundNumber):
+    return ffunctionoutput 
+
+def keyfunc(key):
+    listofkeys = []
     #print("input key:\t\t", key)
     SingleShiftRounds = [1, 2, 9, 16]
-
     keyperm = Permutation(PC, key)
     #print("permkey:\t\t", keyperm)
     leftkey, rightkey = keyperm[:28], keyperm[28:]
@@ -56,25 +55,29 @@ def keyfunc(key, RoundNumber):
     #print("rightkey:\t\t", rightkey)
     leftkey = list(leftkey)
     rightkey = list(rightkey)
-    if any(SingleShiftRounds) == RoundNumber:
-        #print("Single shift")
-        leftkey.append(leftkey.pop(0))
-        rightkey.append(rightkey.pop(0))
-        #print("leftkey:\t\t {}".format(''.join(leftkey)))
-        #print("rightkey:\t\t {}".format(''.join(rightkey)))
-    else:
-        #print("double shift")
-        leftkey.append(leftkey.pop(0))
-        leftkey.append(leftkey.pop(0))
-        rightkey.append(rightkey.pop(0))
-        rightkey.append(rightkey.pop(0))
-        #print("leftkey:\t\t {}".format(''.join(leftkey)))
-        #print("rightkey:\t\t {}".format(''.join(rightkey)))
-    joinkey = leftkey+rightkey
-    subkey = ''.join(joinkey)
-    subkey = Permutation(PC2, subkey)
-    #print("subkey:\t\t\t",subkey)
-    return subkey
+    RoundNumber = 1
+    while RoundNumber <= 16:
+        #print(RoundNumber)
+        if any(SingleShiftRounds) == RoundNumber:
+            #print("Single shift")
+            leftkey.append(leftkey.pop(0))
+            rightkey.append(rightkey.pop(0))
+            #print("leftkey:\t\t {}".format(''.join(leftkey)))
+            #print("rightkey:\t\t {}".format(''.join(rightkey)))
+        else:
+            #print("double shift")
+            leftkey.append(leftkey.pop(0))
+            leftkey.append(leftkey.pop(0))
+            rightkey.append(rightkey.pop(0))
+            rightkey.append(rightkey.pop(0))
+            #print("leftkey:\t\t {}".format(''.join(leftkey)))
+            #print("rightkey:\t\t {}".format(''.join(rightkey)))
+        joinkey = leftkey+rightkey
+        subkey = ''.join(joinkey)
+        subkey = Permutation(PC2, subkey)
+        listofkeys.append(subkey)
+        RoundNumber += 1;
+    return listofkeys
 
 def Permutation(table, bits):
     permbits = []
@@ -84,16 +87,21 @@ def Permutation(table, bits):
     return permbits
 
 if __name__ == '__main__':
+    key = '1101001110101110010101001010101110101011010011101010000000000000'
+    keytextfile = open('key.txt', 'r')
+       
     plaintextfile = open('plain.txt', 'r')
     plaintextbits = ''.join(format(ord(char), 'b') for char in plaintextfile.read())
-    print("Plaintext in bits: "+plaintextbits+"\n")
+    #print("Plaintext in bits: "+plaintextbits+"\n")
     bitlengthcheck = len(plaintextbits) % 64
     if bitlengthcheck != 0:
         print("Input string not correct length must be 64bit blocks")
         print("Last block currently at: ", bitlengthcheck)
     plaintextblocks = [plaintextbits[i:i+64] for i in range(0, len(plaintextbits), 64)]
-    cyphertext = []
+    ciphertext = []
     for block in plaintextblocks:
-        print("input block: \t\t", block)
-        cyphertext.append(EncryptionRounds(Permutation(IP, block)))
-    print(''.join(cyphertext))
+        #print("input block: \t\t", block)
+        ciphertext.append(EncryptionRounds(Permutation(IP, block), key))
+    fullciphertext = ''.join(ciphertext)
+    ciphertext = open('cipher.txt', 'w')
+    ciphertext.write(fullciphertext)

359752b23dff6b617778101cef41809212d058a3
rob <a@b.com>
2019-05-31 23:25:16 -0700

    moved SingleShiftRounds list to varibles.py

--- varibles.py
+++ varibles.py
@@ -92,3 +92,7 @@ P = [16, 7, 20, 21, 29, 12, 28, 17,
     1, 15, 23, 26, 5, 18, 31, 10,
     2, 8, 24, 14, 32, 27, 3, 9,
     19, 13, 30, 6, 22, 11, 4, 25]
+
+#list of rounds that determine if the bit are rotated by one 
+#inside the KeySchedule function
+SingleShiftRounds = [1, 2, 9, 16]

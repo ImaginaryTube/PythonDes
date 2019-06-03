cb5e9d608fc3cd81a3685ed1bba3132d3455cf2f
rob <a@b.com>
2019-06-02 18:13:00 -0700

    removed logging not needed

--- des.py
+++ des.py
@@ -7,73 +7,58 @@ def EncryptionRounds(permblock, key):
     roundnumber = 0
     KeySchedule(key)
     while roundnumber <= 15:
-        #print("Round: ", roundnumber + 1)
-        #print("The Left bits:\t\t",Leftbits)
-        #print("The Right bits:\t\t",Rightbits)
         Leftbits, Rightbits = Rightbits, format((int(Leftbits, 2) ^ int(Ffunction(Rightbits, key[roundnumber]), 2)), '032b')
         roundnumber += 1
     final = Permutation(IPI, Rightbits+Leftbits)
-    #print("output block:\t\t", final)
     return final
         
 def Ffunction(rightbits, subkey):
     sboxinput = Permutation(SE, rightbits)
-    #print("sbox expansion:\t\t", sboxinput)
     sboxinput = int(sboxinput, 2) ^ int(subkey)
     sboxinput = format(sboxinput, '048b')
-    #print("sboxinput:\t\t", sboxinput)
     sboxinputsplit = [sboxinput[i:i+6] for i in range(0, len(sboxinput), 6)]
     sboxnumber = 0
     sboxoutput = []
     for bits in sboxinputsplit:
-        #print("sbox {0} input: {1}".format(sboxnumber+1, bits))
         bits = list(bits)
         row = [bits.pop(0), bits.pop(-1)]
         coloumn = bits
         row = int(''.join(row),2)
         coloumn = int(''.join(coloumn),2)
-        #print("\tcoloumn:",coloumn)
-        #print("\trow:\t",row)
         sboxin = format(Sbox[sboxnumber][row][coloumn], '04b') 
-        #print("\tsboxoutput: ", sboxin)
         sboxoutput.append(sboxin)
         sboxnumber += 1
     ffunctionoutput = Permutation(P, ''.join(sboxoutput))
-    #print("ffunctionoutput output:\t", ffunctionoutput)
+    ("ffunctionoutput output:\t", ffunctionoutput)
     return ffunctionoutput 
 
 def KeySchedule(key):
     listofkeys = []
-    #print("input key:\t\t", key)
-    SingleShiftRounds = [1, 2, 9, 16]
     keyperm = Permutation(PC, key)
-    #print("permkey:\t\t", keyperm)
     leftkey, rightkey = keyperm[:28], keyperm[28:]
-    #print("leftkey:\t\t", leftkey)
-    #print("rightkey:\t\t", rightkey)
     leftkey = list(leftkey)
     rightkey = list(rightkey)
     RoundNumber = 1
     while RoundNumber <= 16:
-        #print(RoundNumber)
         if any(SingleShiftRounds) == RoundNumber:
-            #print("Single shift")
-            leftkey.append(leftkey.pop(0))
-            rightkey.append(rightkey.pop(0))
-            #print("leftkey:\t\t {}".format(''.join(leftkey)))
-            #print("rightkey:\t\t {}".format(''.join(rightkey)))
+            leftkey, rightkey  = KeyEncryption(leftkey, rightkey)
         else:
-            #print("double shift")
-            leftkey.append(leftkey.pop(0))
-            leftkey.append(leftkey.pop(0))
-            rightkey.append(rightkey.pop(0))
-            rightkey.append(rightkey.pop(0))
-            #print("leftkey:\t\t {}".format(''.join(leftkey)))
-            #print("rightkey:\t\t {}".format(''.join(rightkey)))
+            leftkey, rightkey  = KeyEncryption(leftkey, rightkey)
+            leftkey, rightkey  = KeyEncryption(leftkey, rightkey)
         subkey = Permutation(PC2, ''.join(leftkey+rightkey))
         listofkeys.append(subkey)
         RoundNumber += 1;
     return listofkeys
+
+def KeyEncryption(left, right):
+    left.append(left.pop(0))
+    right.append(right.pop(0))
+    return left, right
+    
+def KeyDecryption(left, right):
+    left = [left.pop(-1)] + left
+    right= [right.pop(-1)] + right
+    return left, right
 
 def Permutation(table, bits):
     permbits = []
@@ -86,15 +71,13 @@ if __name__ == '__main__':
     key = bin(int(keytextfile.read(), 16))[2:]
     plaintextfile = open('plain.txt', 'r')
     plaintextbits = ''.join(format(ord(char), 'b') for char in plaintextfile.read())
-    #print("Plaintext in bits: "+plaintextbits+"\n")
     bitlengthcheck = len(plaintextbits) % 64
     if bitlengthcheck != 0:
         print("Input string not correct length must be 64bit blocks")
-        print("Last block currently at: ", bitlengthcheck)
+        print("Last block currently at: {}", bitlengthcheck)
     plaintextblocks = [plaintextbits[i:i+64] for i in range(0, len(plaintextbits), 64)]
     ciphertext = []
     for block in plaintextblocks:
-        #print("input block: \t\t", block)
         ciphertext.append(EncryptionRounds(Permutation(IP, block), key))
     ciphertextfile = ''.join(ciphertext)
     ciphertext = open('cipher.txt', 'w')
